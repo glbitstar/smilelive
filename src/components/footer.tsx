1a888837d2ba0a53dc75ba05b69b349a02822d63
glbitstar <glb.it.star@gmail.com>
2025-05-10 13:40:22 +0900

    [fix] footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -37,25 +37,33 @@ export function Footer() {
 
   return (
     <div
-      className={`fixed bottom-0 z-10 w-full bg-white md:py-2 shadow-[4px_0px_10px_rgba(0,0,0,0.25)] transition-opacity duration-500 ease-in-out ${showFooter ? 'opacity-70' : 'opacity-0 pointer-events-none'
+      className={`fixed bottom-0 z-10 w-full bg-white md:py-2 shadow-[4px_0px_10px_rgba(0,0,0,0.25)] transition-opacity duration-500 ease-in-out ${showFooter ? 'md:opacity-70 opacity-100' : 'opacity-0 pointer-events-none'
         }`}
     >
       <div className='hidden md:flex justify-center items-center gap-5 px-10'>
-        <Link href="" target="_blank" className='w-[450] opacity-70 hover:opacity-100 transition-opacity duration-300' >
-          <img src="/images/footer_line_pc.png" alt="footer_line_pc" className="w-[450]" />
+        <Link href="" target="_blank">
+          <img
+            src="/images/footer_line_pc.png"
+            alt="footer_line_pc"
+            className="w-[450] hover:opacity-100 transition-opacity duration-300"
+          />
         </Link>
-        <Link href="tel:090-0000-0000" target="_blank" className='w-[450] opacity-70 hover:opacity-100 transition-opacity duration-300'>
-          <img src="/images/footer_phone_pc.png" alt="footer_phone_pc" className="w-[450]" />
+        <Link href="tel:090-0000-0000" target="_blank">
+          <img
+            src="/images/footer_phone_pc.png"
+            alt="footer_phone_pc"
+            className="w-[450] hover:opacity-100 transition-opacity duration-300"
+          />
         </Link>
       </div>
       <div className='flex justify-center items-center md:hidden'>
         <div className='flex flex-1 items-center justify-center bg-[#06C755]'>
-          <Link href="" target="_blank" className='w-[15vw] sm:w-[80] opacity-70 hover:opacity-100 transition-opacity duration-300'>
+          <Link href="" target="_blank">
             <img src="/images/footer_line_sp.png" alt="footer_line_sp" className="w-[15vw] sm:w-[80]" />
           </Link>
         </div>
         <div className='flex flex-1 items-center justify-center bg-[#F59DA5]'>
-          <Link href="tel:090-0000-0000" target="_blank" className='w-[15vw] sm:w-[80] opacity-70 hover:opacity-100 transition-opacity duration-300'>
+          <Link href="tel:090-0000-0000" target="_blank">
             <img src="/images/footer_phone_sp.png" alt="footer_phone_sp" className="w-[15vw] sm:w-[80] sm:p-4 p-3" />
           </Link>
         </div>
